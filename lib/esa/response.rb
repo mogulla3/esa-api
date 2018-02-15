c9bb87238fbbadcd53751839ceb05559d6dbfae8
mogulla3 <hs.iarn3@gmail.com>
2018-02-15 21:42:44 +0900

    Use attr_reader

--- lib/esa/response.rb
+++ lib/esa/response.rb
@@ -1,22 +1,12 @@
 module Esa
   class Response
+    attr_reader :code, message, :http_version
+
     def initialize(response)
       @body = response.body
       @code = response.code
       @message = response.message
       @http_version = response.http_version
-    end
-
-    def code
-      @code
-    end
-
-    def message
-      @message
-    end
-
-    def http_version
-      @http_version
     end
 
     def body
