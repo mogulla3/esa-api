0b32cc3dcc8abbf503db3366f8db80d1ba449c38
mogulla3 <hs.iarn3@gmail.com>
2018-02-17 13:12:53 +0900

    Unify method signature

--- lib/esa/client/comment.rb
+++ lib/esa/client/comment.rb
@@ -17,12 +17,12 @@ module Esa
       end
 
       # POST /v1/teams/:team_name/posts/:post_number/comments
-      def post_comment(post_number, params, headers = nil)
+      def create_comment(post_number, params, headers = nil)
         post("/v1/teams/#{team}/posts/#{post_number}/comments", params, headers)
       end
 
       # PATCH /v1/teams/:team_name/comments/:comment_id
-      def patch_comment(comment_id, params, headers = nil)
+      def update_comment(comment_id, params, headers = nil)
         patch("/v1/teams/#{team}/comments/#{comment_id}", params, headers)
       end
 
