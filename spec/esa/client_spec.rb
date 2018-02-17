aaf5d87a8f77dfc34f78b6f5dba7463b4c6d2df7
mogulla3 <hs.iarn3@gmail.com>
2018-02-17 15:16:59 +0900

    Update spec

--- spec/esa/client_spec.rb
+++ spec/esa/client_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe Esa::Client do
-  describe "sample describe" do
-    it "sample it" do
-      expect(1).to eq(1)
+  describe '#initialize' do
+    context 'when team_name is not specified' do
+      it 'raise RuntimeError' do
+      end
     end
+  end
+
+  describe '#get' do
+  end
+
+  describe '#post' do
+  end
+
+  describe '#put' do
+  end
+
+  describe '#patch' do
+  end
+
+  describe '#delete' do
   end
 end
